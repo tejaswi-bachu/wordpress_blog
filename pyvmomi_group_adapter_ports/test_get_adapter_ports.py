8ba54073c519af573fd3f0495403ab1f9ed88dac
Tejaswi Bachu <teja.bachu@gmail.com>
2022-06-30 13:02:32 +0530

    Update test_get_adapter_ports.py

--- pyvmomi_group_adapter_ports/test_get_adapter_ports.py
+++ pyvmomi_group_adapter_ports/test_get_adapter_ports.py
@@ -8,5 +8,4 @@ esxi_host_username = 'root'
 esxi_host_password = 'esxipassword'
 		
 vmware_client = VMWareClient(esxi_host_ip, esxi_host_username, esxi_host_password)	
-adapter_ports = vmware_client.print_adapters_ports()
-print(adapter_ports)
+vmware_client.print_adapters_ports()
