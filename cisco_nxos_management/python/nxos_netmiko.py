e8f40334a1d127c2139ad80bae228a2f2fe2c7dd
Bachu, RaghavaTejaswi <Raghava_tejaswi_Bach@Dell.com>
2022-07-12 12:18:53 +0530

    Add python script to configure NXOS switch using netmiko

---
+++ cisco_nxos_management/python/nxos_netmiko.py
@@ -0,0 +1,31 @@
+import netmiko
+
+from netmiko import ConnectHandler
+
+cisco_nxos_switch = {
+    'device_type': 'cisco_nxos',
+    'host':   '192.168.20.100',
+    'username': 'admin',
+    'password': 'password',
+}
+
+net_connect = ConnectHandler(**cisco_nxos_switch)
+
+
+def configure_interface_ip(interface, ip_addr):
+    commands = [f"interface {interface}", "no switchport", 
+            f"ip address {ip_addr}", "no shutdown", "exit"]
+    output = net_connect.send_config_set(commands)
+    print(output)
+
+def remove_interface_ip(interface, ip_addr):
+    commands = [f"interface {interface}", f"no ip address {ip_addr}", 
+            "switchport", "shutdown", "exit"]
+    output = net_connect.send_config_set(commands)
+    print(output)
+
+
+interface = "Eth1/20"
+ip_addr = "172.168.10.20/24"
+
+remove_interface_ip(interface, ip_addr)
